fd70b2a95a6697ff5b4b724672d408843f505d20
Sergey Minin <sm.minins@gmail.com>
2024-12-14 14:35:45 +0300

    fixation filter_by_state()

---
+++ src/processing.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+
+def filter_by_state(list_of_banking_transactions: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
+    """
+    Принимает список словарей и значение для ключа state и возвращает новый список словарей,
+    содержащий только те словари, у которых ключ соответствует указанному значению.
+    """
+
+    new_list_of_banking_transactions = []
+    for banking_transactions in list_of_banking_transactions:
+        if banking_transactions.get("state") == state:
+            new_list_of_banking_transactions.append(banking_transactions)
+    return new_list_of_banking_transactions
+
+
+if __name__ == "__main__":
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "CANCELED"))

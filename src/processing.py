39f993acdd7d8cc8c709e72f16f2aebd43096265
Sergey Minin <sm.minins@gmail.com>
2024-12-21 20:20:44 +0300

    test_sort_by_date(),test_processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def filter_by_state(list_of_banking_transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает список словарей и значение для ключа state и возвращает новый список словарей,
@@ -16,20 +19,25 @@ def sort_by_date(list_of_transaction_dates: list[dict], date: bool = True) -> li
     Принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание) и возвращает новый список, отсортированный по дате.
     """
+    for transaction_dates in list_of_transaction_dates:
+        dates: Any = transaction_dates.get("date")
+        if len(dates) < 26:
+            raise ValueError("Неизвестный формат даты")
 
     sorted_list_of_transaction_dates = sorted(list_of_transaction_dates, key=lambda i: i.get("date", 0), reverse=date)
     return sorted_list_of_transaction_dates
 
 
-if __name__ == "__main__":
-    print(filter_by_state([
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-        "CANCELED"))
+# if __name__ == "__main__":
+    # print(filter_by_state([
+    #   {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    #   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    #   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    #   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+    #   "CANCELED"))
 
-    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    # print(sort_by_date([
+    #    {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    #    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    #    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    #    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
---
+++ tests/test_processing.py
@@ -0,0 +1,57 @@
+import pytest
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state(list_of_banking_transactions: list[dict]) -> None:
+    '''Тестирование фильтрации списка словарей по статусу state по умолчанию "EXECUTED'''
+
+    assert filter_by_state(list_of_banking_transactions)
+
+
+def test_filter_by_state_set(list_of_banking_transactions: list[dict]) -> None:
+    '''Тестирование фильтрации списка словарей по заданному статусу state CANCELED'''
+
+    assert filter_by_state(list_of_banking_transactions, state="CANCELED")
+
+
+def test_filter_by_no_state(list_of_banking_transactions_no_state: list[dict]) -> None:
+    '''Проверка работы функции при отсутствии словарей с указанным статусом state в списке'''
+
+    assert filter_by_state(list_of_banking_transactions_no_state, 'CANCELED') == []
+
+
+@pytest.mark.parametrize("expected", [
+    ([{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+])
+def test_filter_by_state_parametrize(list_of_banking_transactions: list[dict], expected: list[dict]) -> None:
+    assert filter_by_state(list_of_banking_transactions, state='CANCELED') == expected
+
+
+def test_sort_by_date_decreasing(list_of_banking_transactions: list[dict], date_decreasing: list[dict]) -> None:
+    '''Тестирование сортировки списка словарей по датам в порядке убывания - по умолчанию'''
+
+    assert sort_by_date(list_of_banking_transactions) == date_decreasing
+
+
+def test_sort_by_date_increasing(list_of_banking_transactions: list[dict],
+                                 date_increasing: list[dict], date: bool = False) -> None:
+    '''Тестирование сортировки списка словарей по датам в порядке возрастания'''
+
+    assert sort_by_date(list_of_banking_transactions, date=False) == date_increasing
+
+
+def test_sort_by_date_error_1() -> None:
+    '''Тест на работу функции с некорректными или нестандартными форматами дат'''
+    with pytest.raises(ValueError):
+        assert sort_by_date(
+            [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18'}]
+        ) == "Неизвестный формат даты"
+
+
+def test_sort_by_date_error_2() -> None:
+    '''Тест на работу функции с некорректными или нестандартными форматами дат'''
+    with pytest.raises(ValueError):
+        assert sort_by_date(
+            [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019'}]
+        ) == "Неизвестный формат даты"

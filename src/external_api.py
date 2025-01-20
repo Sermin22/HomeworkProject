e4c11778962fc47e830bd7fda37d9d6e02d1c7d5
Sergey Minin <sm.minins@gmail.com>
2025-01-20 04:59:35 +0300

    добавлены модуль utils и external_api с функциями

---
+++ src/external_api.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+import requests
+from src.utils import list_financial_transactions
+
+
+def conversion_to_rubles(transactions: list[dict]) -> list:
+    '''Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+    Если транзакция была в USD или EUR , происходит обращение к внешнему API для получения текущего
+    курса валют и конвертации суммы операции в рубли.'''
+
+    list_amount = []
+    for transaction in transactions:
+        try:
+            if transaction["operationAmount"]["currency"]["code"] == "RUB":
+                amount_rub = float(transaction["operationAmount"]["amount"])
+                list_amount.append(amount_rub)
+            if transaction["operationAmount"]["currency"]["code"] != "RUB":
+                from_code = transaction["operationAmount"]["currency"]["code"]  # Трехбуквенный код валюты,
+                # которую вы хотите конвертировать
+                to = "RUB"  # Трехбуквенный код валюты, в которую вы хотите конвертировать
+                amount = float(transaction["operationAmount"]["amount"])  # Сумма, подлежащая конвертации
+                load_dotenv()
+                APIlayer_KEY = os.getenv('APIlayer_KEY')
+                headers = {
+                    "apikey": APIlayer_KEY
+                }
+                url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_code}&amount={amount}"
+                response = requests.get(url, headers=headers)
+                if response.status_code == 200:
+                    result = response.json()['result']
+                    result = round(float(result), 4)
+                    list_amount.append(result)
+                else:
+                    print(f"Конвертация валюты неуспешна. Возможная причина: {response.reason}")
+        except KeyError:
+            continue
+    return list_amount
+
+
+if __name__ == "__main__":
+    transactions = list_financial_transactions("../data/transactions.json")
+    print(conversion_to_rubles(transactions))
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+from typing import Any
+import json
+
+
+def list_financial_transactions(file_json: str) -> list[dict[Any, Any]]:
+    '''Принимает на вход путь до JSON-файла и возвращает список словарей с данными о
+    финансовых транзакциях'''
+    try:
+        with open(file_json, encoding="utf-8") as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                return []
+    except FileNotFoundError:
+        return []
+    if data and isinstance(data, list):
+        return data
+    else:
+        return []
+
+
+if __name__ == "__main__":
+    print(list_financial_transactions("../data/operations.json"))
